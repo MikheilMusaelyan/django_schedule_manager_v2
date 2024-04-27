e231187fa2982385b712b15b3ecec2727ee341c4
Mishka Musaelyan <mishkamusaelyan@mishkas-mbp.mynetworksettings.com>
2024-04-27 00:23:37 -0400

    debug=false

--- django_project/settings.py
+++ django_project/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "hadasdasdassdas"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
